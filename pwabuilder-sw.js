1c1a409272605d1a31bdc950a014354eaef96bbf
ntnk1984 <ntnk1984@gmail.com>
2019-09-16 11:08:43 +0700

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,7 +1,7 @@
 //This is the "Offline page" service worker
 const cachName = 'static-shell-v1';
-const resourceToCache = ['/'
-                         , '/Tinda.html'
+const resourceToCache = ['/theface'
+                         , '/theface/Tinda.html'
                         , './fancybox/jquery.mousewheel-3.0.4.pack.js'
                          ,'./fancybox/jquery.fancybox-1.3.4.pack.js'
                          , './fancybox/jquery.fancybox-1.3.4.css'
